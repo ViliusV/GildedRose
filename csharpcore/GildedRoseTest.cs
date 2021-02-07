0a8df580711c3bc808f2b8076fa427533a1326b6
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-02-07 21:24:21 +0200

    TDD - refactored successful tests

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -9,80 +9,209 @@ namespace csharpcore
 		public void Should_DecreaseQualityTwiceAsFast_When_SellByHasPassed()
 		{
 			var qualityBefore = 10;
-			var qualityAfter = 8;
-			var qualityChange = qualityAfter - qualityBefore;
-			Assert.Equal(-2, qualityChange);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Dummy generic item",
+					SellIn = 0,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.Equal(-2, items[0].Quality - qualityBefore);
 		}
 
 		[Fact]
 		public void ShouldNot_HaveNegativeQuality()
 		{
-			var quality = 0;
-			Assert.True(quality >= 0);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Dummy generic item",
+					SellIn = 5,
+					Quality = 0
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.True(items[0].Quality >= 0);
 		}
 
 		[Fact]
-		public void Should_InceaseQuality_When_ItemIsAgedBrie()
+		public void Should_IncreaseQuality_When_ItemIsAgedBrie()
 		{
 			var qualityBefore = 10;
-			var qualityAfter = 11;
-			Assert.True(qualityAfter > qualityBefore);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Aged Brie",
+					SellIn = 5,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.True(items[0].Quality > qualityBefore);
 		}
 
 		[Fact]
 		public void ShouldNot_HaveQualityHigherThan50()
 		{
-			var quality = 10;
-			Assert.True(quality <= 50);
+			var qualityBefore = 50; //ToDo - validate when creating objects if quality is in correct range
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Aged Brie",
+					SellIn = 5,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.True(items[0].Quality <= 50); // ToDo - Move 50, 0 to consts
 		}
 		[Fact]
 		public void ShouldNot_BeSold_When_ItemIsSulfuras()
 		{
 			var sellInBefore = 5;
-			var sellInAfter = 5;
-			Assert.Equal(sellInAfter, sellInBefore);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Sulfuras, Hand of Ragnaros",
+					SellIn = sellInBefore,
+					Quality = 10
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.Equal(sellInBefore, items[0].SellIn);
 		}
 
 		[Fact]
 		public void ShouldNotDecreaseQuality_When_ItemIsSulfuras()
 		{
-			//ToDo - Have 2 tests where quality increases and where it stays the same (e.g., when quality is max-quality - 50) 	
 			var qualityBefore = 10;
-			var qualityAfter = 10;
-			Assert.True(qualityAfter >= qualityBefore);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Sulfuras, Hand of Ragnaros",
+					SellIn = 5,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.True(items[0].Quality >= qualityBefore);
 		}
 
 		[Fact]
 		public void Should_IncreaseQuality_When_ItemIsBackstagePasses_And_SellByDateIsApproaching()
 		{
 			var qualityBefore = 10;
-			var qualityAfter = 11;
-			Assert.True(qualityAfter > qualityBefore);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Backstage passes to a TAFKAL80ETC concert",
+					SellIn = 15,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.True(items[0].Quality > qualityBefore);
 		}
 
-		[Fact]
-		public void Should_IncreaseQualityBy2_When_ItemIsBackstagePasses_And_SellByDateIsIn10DaysOrLess()
+		[Theory]
+		[InlineData(10)]
+		[InlineData(9)]
+		[InlineData(8)]
+		[InlineData(7)]
+		[InlineData(6)]
+		public void Should_IncreaseQualityBy2_When_ItemIsBackstagePasses_And_SellByDateIsIn10DaysOrLess(int sellIn)
 		{
 			var qualityBefore = 10;
-			var qualityAfter = 12;
-			var qualityChange = qualityAfter - qualityBefore;
-			Assert.Equal(2, qualityChange);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Backstage passes to a TAFKAL80ETC concert",
+					SellIn = sellIn,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.Equal(2, items[0].Quality - qualityBefore);
 		}
 
-		[Fact]
-		public void Should_IncreaseQualityBy3_When_ItemIsBackstagePasses_And_SellByDateIsIn5DaysOrLess()
+		[Theory]
+		[InlineData(5)]
+		[InlineData(4)]
+		[InlineData(3)]
+		[InlineData(2)]
+		[InlineData(1)]
+		public void Should_IncreaseQualityBy3_When_ItemIsBackstagePasses_And_SellByDateIsIn5DaysOrLess(int sellIn)
 		{
 			var qualityBefore = 10;
-			var qualityAfter = 13;
-			var qualityChange = qualityAfter - qualityBefore;
-			Assert.Equal(3, qualityChange);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Backstage passes to a TAFKAL80ETC concert",
+					SellIn = sellIn,
+					Quality = qualityBefore
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.Equal(3, items[0].Quality - qualityBefore);
 		}
 
-		[Fact]
-		public void Should_SetQualityTo0_When_ItemIsBackstagePasses_And_ConcertHasPassed()
+		[Theory]
+		[InlineData(0)]
+		[InlineData(-1)]
+		public void Should_SetQualityTo0_When_ItemIsBackstagePasses_And_ConcertHasPassed(int sellIn)
 		{
-			var qualityAfter = 0;
-			Assert.Equal(0, qualityAfter);
+			var items = new List<Item>
+			{
+				new Item
+				{
+					Name = "Backstage passes to a TAFKAL80ETC concert",
+					SellIn = sellIn,
+					Quality = 10
+				}
+			};
+			var app = new GildedRose(items);
+
+			app.UpdateQuality();
+
+			Assert.Equal(0, items[0].Quality);
 		}
 	}
 }
