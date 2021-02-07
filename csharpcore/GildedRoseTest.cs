862833ba3eb1a666ecf7bc91f55adfde87e9d532
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-02-07 20:21:18 +0200

    TDD - failing tests

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -3,15 +3,65 @@ using System.Collections.Generic;
 
 namespace csharpcore
 {
-    public class GildedRoseTest
-    {
-        [Fact]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
-        }
-    }
+	public class GildedRoseTest
+	{
+		[Fact]
+		public void Should_DecreaseQualityTwiceAsFast_When_SellByHasPassed()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void ShouldNot_HaveNegativeQuality()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void Should_InceaseQuality_When_ItemIsAgedBrie()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void ShouldNot_HaveQualityHigherThan50()
+		{
+			Assert.True(false);
+		}
+		[Fact]
+		public void ShouldNot_BeSold_When_ItemIsSulfuras()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void ShouldNotDecreaseQuality_When_ItemIsSulfuras()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void Should_IncreaseQuality_When_ItemIsBackstagePasses_And_SellByDateIsApproaching()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void Should_IncreaseQualityBy2_When_ItemIsBackstagePasses_And_SellByDateIsIn10DaysOrLess()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void Should_IncreaseQualityBy3_When_ItemIsBackstagePasses_And_SellByDateIsIn5DaysOrLess()
+		{
+			Assert.True(false);
+		}
+
+		[Fact]
+		public void Should_SetQualityTo0_When_ItemIsBackstagePasses_And_ConcertHasPassed()
+		{
+			Assert.True(false);
+		}
+	}
 }
