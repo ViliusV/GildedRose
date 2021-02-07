a1e2f9b8f1c5f8804aeb9e6a35338e652a55ebdf
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-02-07 22:00:33 +0200

    Refactor - merge else with if

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -2,83 +2,79 @@
 
 namespace csharpcore
 {
-    // ToDo - Make Static?
-    public class GildedRose
-    {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items)
-        {
-            this.Items = Items;
-        }
+	// ToDo - Make Static?
+	public class GildedRose
+	{
+		IList<Item> Items;
+		public GildedRose(IList<Item> Items)
+		{
+			this.Items = Items;
+		}
 
-        public void UpdateQuality()
-        {
-            //ToDo - reread requirements
-            //ToDo - add Conjured items
+		public void UpdateQuality()
+		{
+			//ToDo - reread requirements
+			//ToDo - add Conjured items
 
-            foreach (var item in Items)
-            {
-                // Step 1 - Update Quality
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
+			foreach (var item in Items)
+			{
+				// Step 1 - Update Quality
+				if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+				{
 					if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
 					{
 						item.Quality--;
 					}
 				}
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality++;
+				else if (item.Quality < 50)
+				{
+					item.Quality++;
 
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-							if (item.SellIn < 11 && item.Quality < 50)
-							{
-								item.Quality++;
-							}
-
-							if (item.SellIn < 6 && item.Quality < 50)
-							{
-								item.Quality++;
-							}
+					if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+					{
+						if (item.SellIn <= 10 && item.Quality < 50)
+						{
+							item.Quality++;
 						}
-                    }
-                }
 
-                // Step 2 - Reduce Sell In
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn--;
-                }
+						if (item.SellIn <= 5 && item.Quality < 50)
+						{
+							item.Quality++;
+						}
+					}
+				}
 
-                // Step 3 - Update quality for Items after Sell By date has passed
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
+
+				// Step 2 - Reduce Sell In
+				if (item.Name != "Sulfuras, Hand of Ragnaros")
+				{
+					item.SellIn--;
+				}
+
+				// Step 3 - Update quality for Items after Sell By date has passed
+				if (item.SellIn < 0)
+				{
+					if (item.Name != "Aged Brie")
+					{
+						if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+						{
 							if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
 							{
 								item.Quality--;
 							}
 						}
-                        else
-                        {
-                            item.Quality = 0;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality++;
-                        }
-                    }
-                }
-            }
-        }
-    }
+						else
+						{
+							item.Quality = 0;
+						}
+					}
+					else if (item.Quality < 50)
+					{
+						item.Quality++;
+					}
+
+				}
+			}
+		}
+	}
 }
