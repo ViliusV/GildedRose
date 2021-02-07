d9f4233c1502bafbef66251889174222b9afc9b9
Vilius Vaicekauskas <vilius.vaicekauskas@outlook.com>
2021-02-07 20:34:44 +0200

    TDD - dummy successful tests

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -8,60 +8,81 @@ namespace csharpcore
 		[Fact]
 		public void Should_DecreaseQualityTwiceAsFast_When_SellByHasPassed()
 		{
-			Assert.True(false);
+			var qualityBefore = 10;
+			var qualityAfter = 8;
+			var qualityChange = qualityAfter - qualityBefore;
+			Assert.Equal(-2, qualityChange);
 		}
 
 		[Fact]
 		public void ShouldNot_HaveNegativeQuality()
 		{
-			Assert.True(false);
+			var quality = 0;
+			Assert.True(quality >= 0);
 		}
 
 		[Fact]
 		public void Should_InceaseQuality_When_ItemIsAgedBrie()
 		{
-			Assert.True(false);
+			var qualityBefore = 10;
+			var qualityAfter = 11;
+			Assert.True(qualityAfter > qualityBefore);
 		}
 
 		[Fact]
 		public void ShouldNot_HaveQualityHigherThan50()
 		{
-			Assert.True(false);
+			var quality = 10;
+			Assert.True(quality <= 50);
 		}
 		[Fact]
 		public void ShouldNot_BeSold_When_ItemIsSulfuras()
 		{
-			Assert.True(false);
+			var sellInBefore = 5;
+			var sellInAfter = 5;
+			Assert.Equal(sellInAfter, sellInBefore);
 		}
 
 		[Fact]
 		public void ShouldNotDecreaseQuality_When_ItemIsSulfuras()
 		{
-			Assert.True(false);
+			//ToDo - Have 2 tests where quality increases and where it stays the same (e.g., when quality is max-quality - 50) 	
+			var qualityBefore = 10;
+			var qualityAfter = 10;
+			Assert.True(qualityAfter >= qualityBefore);
 		}
 
 		[Fact]
 		public void Should_IncreaseQuality_When_ItemIsBackstagePasses_And_SellByDateIsApproaching()
 		{
-			Assert.True(false);
+			var qualityBefore = 10;
+			var qualityAfter = 11;
+			Assert.True(qualityAfter > qualityBefore);
 		}
 
 		[Fact]
 		public void Should_IncreaseQualityBy2_When_ItemIsBackstagePasses_And_SellByDateIsIn10DaysOrLess()
 		{
-			Assert.True(false);
+			var qualityBefore = 10;
+			var qualityAfter = 12;
+			var qualityChange = qualityAfter - qualityBefore;
+			Assert.Equal(2, qualityChange);
 		}
 
 		[Fact]
 		public void Should_IncreaseQualityBy3_When_ItemIsBackstagePasses_And_SellByDateIsIn5DaysOrLess()
 		{
-			Assert.True(false);
+			var qualityBefore = 10;
+			var qualityAfter = 13;
+			var qualityChange = qualityAfter - qualityBefore;
+			Assert.Equal(3, qualityChange);
 		}
 
 		[Fact]
 		public void Should_SetQualityTo0_When_ItemIsBackstagePasses_And_ConcertHasPassed()
 		{
-			Assert.True(false);
+			var qualityAfter = 0;
+			Assert.Equal(0, qualityAfter);
 		}
 	}
 }
